2b84bf8715e72173dee9ab8320e2c9ec1912a148
MorpKnight <christoffelsihombing@gmail.com>
2026-06-15 20:42:25 +0700

    Initial Commit

---
+++ Worthly/WorthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorthlyApp.swift
+//  Worthly
+//
+//  Created by Giovan Christoffel Sihombing on 2026/06/15.
+//
+
+import SwiftUI
+
+@main
+struct WorthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
